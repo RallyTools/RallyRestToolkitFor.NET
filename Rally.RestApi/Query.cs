253cc36c6ed9346afac9bf791e64af95d4ae3c74
Scott Lehman <scott@appmeum.com>
2014-02-28 08:03:38 -0700

    Documentation changes

--- Rally.RestApi/Query.cs
+++ Rally.RestApi/Query.cs
@@ -241,8 +241,6 @@ namespace Rally.RestApi
 				}
 				const char quote = '"';
 				// Quote the value
-				// Removed due to issues of double encoding, left for reference in case something breaks.
-				// var outValue = quote + Value.Replace("%", "%25").Replace("&", "%26").Replace("#", "%23").Replace("\"", "%22").Replace("+", "%2B") + quote;
 				var outValue = quote + Value + quote;
 				return string.Format("({0} {1} {2})", Attribute, GetOperator(QueryOperator), outValue);
 			}
